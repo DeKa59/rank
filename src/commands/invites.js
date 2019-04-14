7740407ac97b68e982feed321e1e01d46c72fd7b
DeKa59 <49624455+DeKa59@users.noreply.github.com>
2019-04-15 01:28:50 +0200

    Add files via upload

--- src/commands/invites.js
+++ src/commands/invites.js
@@ -1,17 +1,26 @@
-import Discord from 'discord.js';
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _discord = require('discord.js');
+
+var _discord2 = _interopRequireDefault(_discord);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 const invites = (msg, numberUses, numberLeft, nextRole) => {
-  const richEmbed = new Discord.RichEmbed();
+  const richEmbed = new _discord2.default.RichEmbed();
   msg.channel.send({
-    embed: richEmbed
-            .setColor('#ffffff')
-            .setDescription(`**Stats for ${msg.author}**
+    embed: richEmbed.setColor('#ffffff').setDescription(`**Stats for ${msg.author}**
 
-:ballot_box_with_check: Users Invited: ${numberUses}
-:trophy: Current Rank: ${msg.member.highestRole}
-:chart_with_upwards_trend: Invites left: ${numberLeft}
-:level_slider: Next Rank: ${nextRole}`)
+:ballot_box_with_check: Nombres d'invitations: ${numberUses}
+:trophy: Rank Actuel: ${msg.member.highestRole}
+:chart_with_upwards_trend: Invitations Restantes: ${numberLeft}
+:level_slider: Rank Suivant: ${nextRole}
+N'oublie pas de créer une invitation permanente.`)
   });
-}
+};
 
-export default invites;
+exports.default = invites;
