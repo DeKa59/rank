a32e0a39e33b87321cccadb73a280dd3850f3b97
DeKa59 <49624455+DeKa59@users.noreply.github.com>
2019-04-15 01:29:02 +0200

    Add files via upload

--- src/commands/top.js
+++ src/commands/top.js
@@ -1,36 +1,41 @@
-import Discord from 'discord.js';
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _discord = require('discord.js');
+
+var _discord2 = _interopRequireDefault(_discord);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 const top = (bot, msg, cmd, subcmd) => {
-  const richEmbed = new Discord.RichEmbed();
+  const richEmbed = new _discord2.default.RichEmbed();
 
-  let allInvites = msg.guild.fetchInvites()
-    .then(result => {
-      let sortedInvites = result.array().sort((a, b) => {
-        if (a.uses === b.uses) return 0;
-        // sort a to lower index than b if a.uses > b.uses
-        return a.uses > b.uses ? -1 : 1;
-      });
-
-      let largest = subcmd > sortedInvites.length ? sortedInvites.length : subcmd;
-      let number = largest > 10 ? 10 : largest;
-      let completeString = '';
-      for (let i = 0; i < number; i++) {
-        let rankString = `${i+1}. Invites: ${sortedInvites[i].uses} - ${sortedInvites[i].inviter.username} \n`;
-        completeString += rankString;
-      }
-      if (subcmd > 10) {
-        msg.channel.send({
-          embed: richEmbed
-                  .setColor('#ffffff')
-                  .setDescription(`The maximum number is 10. \nHere are the top 10:`)
-        });
-      }
-      msg.channel.send({
-        embed: richEmbed
-                .setColor('#ffffff')
-                .setDescription(`${completeString}`)
-      });
+  let allInvites = msg.guild.fetchInvites().then(result => {
+    let sortedInvites = result.array().sort((a, b) => {
+      if (a.uses === b.uses) return 0;
+      // sort a to lower index than b if a.uses > b.uses
+      return a.uses > b.uses ? -1 : 1;
     });
-}
 
-export default top;
+    let largest = subcmd > sortedInvites.length ? sortedInvites.length : subcmd;
+    let number = largest > 10 ? 10 : largest;
+    let completeString = '';
+    for (let i = 0; i < number; i++) {
+      let rankString = `${i + 1}. Invites: ${sortedInvites[i].uses} - ${sortedInvites[i].inviter.username} \n`;
+      completeString += rankString;
+    }
+    if (subcmd > 10) {
+      msg.channel.send({
+        embed: richEmbed.setColor('#ffffff').setDescription(`The maximum number is 10. \nHere are the top 10:`)
+      });
+    }
+    msg.channel.send({
+      embed: richEmbed.setColor('#ffffff').setDescription(`${completeString}`)
+    });
+  });
+};
+
+exports.default = top;
